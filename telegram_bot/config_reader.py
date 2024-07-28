4c86d78e702daf265bc3ed605f812cc5a36d8e21
danya <danny.glazovs@gmail.com>
2024-07-28 11:11:07 +0300

    add SettingsConfigDict with env_prefix

--- telegram_bot/config_reader.py
+++ telegram_bot/config_reader.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr, PostgresDsn
 
 
@@ -7,3 +7,4 @@ class Settings(BaseSettings):
     bot_token: SecretStr = "6235035727:AAEeeliZcjeNobBAuEjAmXGexNjxkMJVMCo"
     pg_dsn: PostgresDsn = "postgres://postgres:password@postgres:5432"
     weather_API: SecretStr = "fc99a79328de8b7fa62cec28ebbb5a00"
+    model_config = SettingsConfigDict(env_prefix='_')
