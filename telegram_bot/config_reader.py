ff38bab0377d32875cc7ca036e7fd2f72266fd43
danya <danny.glazovs@gmail.com>
2024-07-28 11:20:42 +0300

    experiment with .env

--- telegram_bot/config_reader.py
+++ telegram_bot/config_reader.py
@@ -2,9 +2,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr, PostgresDsn
 
 
-# Settings for telegram bot, database (postgres) and weather
+# Settings for telegram bot, database (postgres and weather
 class Settings(BaseSettings):
-    bot_token: SecretStr = "6235035727:AAEeeliZcjeNobBAuEjAmXGexNjxkMJVMCo"
-    pg_dsn: PostgresDsn = "postgres://postgres:password@postgres:5432"
-    weather_API: SecretStr = "fc99a79328de8b7fa62cec28ebbb5a00"
-    model_config = SettingsConfigDict(env_prefix='_')
+    bot_token: SecretStr
+    pg_dsn: PostgresDsn
+    weather_API: SecretStr
+    model_config: SettingsConfigDict = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
+
+
+config = Settings(
+    bot_token="6235035727:AAEeeliZcjeNobBAuEjAmXGexNjxkMJVMCo",  # Telegram bot API TOKEN
+    pg_dsn="postgres://postgres:password@postgres:5432",
+    weather_API="fc99a79328de8b7fa62cec28ebbb5a00"
+)
