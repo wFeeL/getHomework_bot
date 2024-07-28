d0711ad88fc18ca1af4313e8fc9b4876a668ddc2
danya <danny.glazovs@gmail.com>
2024-07-28 10:57:32 +0300

    delete confidential information about telegram bot and project; update config file

--- telegram_bot/config_reader.py
+++ telegram_bot/config_reader.py
@@ -1,24 +1,9 @@
 from pydantic_settings import BaseSettings
 from pydantic import SecretStr, PostgresDsn
 
-# Database connection config
-DB_CONFIG = {
-    'host': 'postgres',
-    'user': 'postgres',
-    'password': 'password',
-    'port': 5432
-}
 
-
+# Settings for telegram bot, database (postgres) and weather
 class Settings(BaseSettings):
-    bot_token: SecretStr
-    pg_dsn: PostgresDsn
-    weather_API: SecretStr
-
-
-# Settings for telegram bot, postgres and weather
-config = Settings(
-    bot_token="",  # Telegram bot API TOKEN
-    pg_dsn=f"postgres://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}",
-    weather_API='fc99a79328de8b7fa62cec28ebbb5a00'
-)
+    bot_token: SecretStr = "6235035727:AAEeeliZcjeNobBAuEjAmXGexNjxkMJVMCo"
+    pg_dsn: PostgresDsn = "postgres://postgres:password@postgres:5432"
+    weather_API: SecretStr = "fc99a79328de8b7fa62cec28ebbb5a00"
