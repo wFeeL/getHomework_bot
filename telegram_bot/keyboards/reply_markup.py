479e8c7a2be164a9e7350b17862a9f338b1b07a7
danya <danny.glazovs@gmail.com>
2024-08-07 08:52:42 +0300

    add reply keyboard for /textbook

--- telegram_bot/keyboards/reply_markup.py
+++ telegram_bot/keyboards/reply_markup.py
@@ -12,3 +12,18 @@ def get_subjects_keyboard() -> ReplyKeyboardMarkup:
         builder.row(KeyboardButton(text=elem[0]))
 
     return builder.as_markup()
+
+
+def get_pdf_page_keyboard(page: int | str) -> ReplyKeyboardMarkup:
+    if page.isdigit():
+        markup = ReplyKeyboardMarkup(
+            keyboard=[[KeyboardButton(text=f'{page - 1}'), KeyboardButton(text=f'{page + 1}')]]
+        )
+    else:
+        page = page.split('.')
+        paragraph, number = int(page[0]), int(page[1])
+        markup = ReplyKeyboardMarkup(
+            keyboard=[[KeyboardButton(text=f'{paragraph}.{number - 1}'),
+                       KeyboardButton(text=f'{paragraph}.{number + 1}')]]
+        )
+    return markup
