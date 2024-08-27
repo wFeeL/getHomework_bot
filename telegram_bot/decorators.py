d12fc0bf372173c78f2a9d2f00a9bf2f7ecc444d
danya <danny.glazovs@gmail.com>
2024-08-27 19:29:25 +0300

    add check class decorator

--- telegram_bot/decorators.py
+++ telegram_bot/decorators.py
@@ -1,15 +1,26 @@
-from telegram_bot.database_methods.database_request import get_users
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+
+from telegram_bot.database_methods.database_request import get_admins, get_users
+from telegram_bot.handlers import bot_commands
 
 
 # Decorator for checking admin's of bot
 def check_admin(func):
-    async def wrapper_check_admin(message: Message, state: FSMContext) -> None:
-        telegram_id = str(message.chat.id)
-        admin_list = list(map(lambda x: x[0], get_users(is_admin=True)))
-
-        if telegram_id in admin_list:
-            await func(message, state)
-
+    async def wrapper_check_admin(message: Message, **kwargs) -> None:
+        if get_admins(telegram_id=message.chat.id)[0][2]:
+            await func(message, **kwargs)
+        else:
+            await bot_commands.handle_text(message, **kwargs)
     return wrapper_check_admin
+
+
+# Decorator for checking class of users
+def check_class(func):
+    async def wrapper_check_class(message: Message, **kwargs) -> None:
+        if get_users(telegram_id=message.chat.id)[0][1] == 0:
+            await bot_commands.send_class(message, **kwargs)
+        else:
+            await func(message, **kwargs)
+
+    return wrapper_check_class
