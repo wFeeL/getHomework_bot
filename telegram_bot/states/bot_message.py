448f9037612d7f8762bd47723286d0dcaa37e4c1
danya <danny.glazovs@gmail.com>
2024-12-31 11:50:33 +0300

    add a state for /bot_message command

---
+++ telegram_bot/states/bot_message.py
@@ -0,0 +1,54 @@
+from aiogram import Router, F, Bot
+from aiogram.client.bot import DefaultBotProperties
+from aiogram.filters.callback_data import CallbackData
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardRemove
+from aiogram_calendar import SimpleCalendar, SimpleCalendarCallback
+
+from telegram_bot import text_message
+from telegram_bot.config_reader import config
+from telegram_bot.database_methods.database_request import *
+from telegram_bot.keyboards import inline_markup, reply_markup
+
+
+# Form for message data
+class MessageForm(StatesGroup):
+    text = State()
+
+
+router = Router()
+bot = Bot(config.bot_token, default=DefaultBotProperties(parse_mode='HTML'))
+
+
+# Start registering a bot's message
+async def register_message(message: Message, state: FSMContext) -> None:
+    await message.answer(text=text_message.BOT_MESSAGE_INPUT_TEXT)
+    await state.set_state(MessageForm.text)
+
+
+# Process message text
+@router.message(MessageForm.text)
+async def process_message_text(message: Message, state: FSMContext) -> None:
+    text_to_send = message.text
+    await state.update_data(text=text_to_send)
+    await message.answer(text=text_message.BOT_MESSAGE_TEXT.format(message=text_to_send),
+                         reply_markup=inline_markup.get_send_message_keyboard())
+
+
+
+# Send message to all users
+@router.callback_query(F.data == 'send_message')
+async def send_message(callback: CallbackQuery, state: FSMContext) -> None:
+    await callback.message.delete()
+    class_id = get_admins(telegram_id=callback.message.chat.id)[0][1]
+
+    users_list = get_users(class_id=class_id)
+    data = await state.get_data()
+    text_to_send = data['text']
+    await state.clear()
+
+    for telegram_id in list(map(lambda elem: elem[0], users_list)):
+        await bot.send_message(chat_id=telegram_id, text=text_to_send)
+
+    await callback.message.answer(text=text_message.BOT_MESSAGE_SUCCESS)
